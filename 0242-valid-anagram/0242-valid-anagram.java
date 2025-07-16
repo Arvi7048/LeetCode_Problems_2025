c148b1f541bcbb8a49f6fe7d63a559c3fc71c831
Arvind Gautam <iitarvindgautam@gmail.com>
2025-07-16 12:22:35 +0530

    Time: 18 ms (15.35%), Space: 44.9 MB (25.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        // Count frequency of each character in s
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        int[] freq = new int[26]; // 26 letters of the alphabet
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++; // Increase count for s
-            freq[t.charAt(i) - 'a']--; // Decrease count for t
-        }
-
-        // If all values are 0, it's an anagram
-        for (int count : freq) {
-            if (count != 0) {
-                return false;
-            }
+        // Subtract frequency based on t
+        for (char ch : t.toCharArray()) {
+            if (!map.containsKey(ch)) return false;
+            map.put(ch, map.get(ch) - 1);
+            if (map.get(ch) < 0) return false;
         }
 
         return true;
