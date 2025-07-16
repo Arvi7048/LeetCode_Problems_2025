46350a5d0706875723e259706b26e67b19668ae3
Arvind Gautam <iitarvindgautam@gmail.com>
2025-07-16 12:22:20 +0530

    Time: 5 ms (56.64%), Space: 43 MB (82.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!= t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
-        int n = s.length();
-        char[] sa = s.toCharArray();
-        char[] ta = t.toCharArray();
-        Arrays.sort(sa);
-        Arrays.sort(ta);
-        return Arrays.equals(sa,ta);
-        
 
+        int[] freq = new int[26]; // 26 letters of the alphabet
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++; // Increase count for s
+            freq[t.charAt(i) - 'a']--; // Decrease count for t
+        }
+
+        // If all values are 0, it's an anagram
+        for (int count : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
